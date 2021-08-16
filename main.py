bd5c6e71730b9f8be0056f85473a85907c396b8f
CantChooseThis <cant@choosethis.org>
2021-08-16 16:01:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ WIN = pg.display.set_mode((WIDTH, HEIGHT))
 
 def draw(*args):
     WIN.fill((128, 128, 128))
-    WIN.blit(pg.image.load("Pepperoni.png"), pg.image.load("Pepperoni.png").get_rect(x=0, y=0))
     for arg in args:
         WIN.blit(arg.surface, arg.rect)
     pg.display.update()
@@ -21,26 +20,29 @@ def draw(*args):
 
 def main():
     offset = 64
-    pepperoni = Button((26, 26))
-    mushroom = Button((26+offset*1, 26))
-    oliveB = Button((26+offset*2, 26))
-    oliveG = Button((26+offset*3, 26))
-    pineapple = Button((26+offset*4, 26))
-    sausage = Button((26+offset*5, 26))
+    pepperoni = Button((26, 26), "Pepperoni")
+    mushroom = Button((26+offset*1, 26), "Mushroom_Red")
+    oliveB = Button((26+offset*2, 26), "Olive_Black")
+    oliveG = Button((26+offset*3, 26), "Olive_Green")
+    pineapple = Button((26+offset*4, 26), "Pineapple")
+    sausage = Button((26+offset*5, 26), "Sausage")
     buttons = [pepperoni, mushroom, oliveB, oliveG, pineapple, sausage]
-    toppings = ["Pepperoni.png", "Mushroom_Red.png", "Olive_Black.png", "Olive_Green.png", "Pineapple.png", "Sausage.png"]
-    for x in range(len(toppings)):
-        img = pg.image.load(toppings[x])
-        # buttons[x].surface.blit(img, img.get_rect(x=0, y=0))
+    toppings = []
     play = True
     printed = False
     while play:
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 play = False
-        if pg.mouse.get_pressed()[0] and pepperoni.rect.collidepoint(pg.mouse.get_pos()):
-            pepperoni.rect.center = pg.mouse.get_pos()
-        draw(*buttons)
+        if pg.mouse.get_pressed()[0]:
+            for button in buttons:
+                if button.rect.collidepoint(pg.mouse.get_pos()):
+                    topping = button.copy()
+                    toppings.append(topping)
+
+                    pg.mouse.set_pos(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1]+50)
+
+        draw(*buttons, *toppings)
         if pg.mouse.get_pressed()[2] and not printed:
             printed = True
             print([button.rect.center for button in buttons])
